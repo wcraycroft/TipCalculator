cbd5bc11a9da940119511edc8d5c34732b59a312
wcraycroft <wrcraycroft@gmail.com>
2019-02-05 19:29:15 -0800

    Crash fix (Parsing empty String). Cleaned up comments.

--- app/src/main/java/edu/miracosta/cs134/wcraycroft/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/miracosta/cs134/wcraycroft/tipcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -17,8 +18,10 @@ public class MainActivity extends AppCompatActivity {
     NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
     NumberFormat percent = NumberFormat.getPercentInstance(Locale.getDefault());
 
-    // Member variables for each component used in the app
+    // Declare model
     private Bill currentBill;
+
+    // Member variables for each component used in the app
     private EditText amountEditText;
     private TextView percentTextView;
     private TextView tipTextView;
@@ -42,16 +45,16 @@ public class MainActivity extends AppCompatActivity {
         // Set tip percentage to match seek bar
         currentBill.setTipPercent(percentSeekBar.getProgress() / 100.0);
 
+        // SeekBar change event
         percentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // Update model
                 currentBill.setTipPercent(progress / 100.0);
-                // Change label of tip percent
+                // Change label of tip percent, tip amount and total amount
                 percentTextView.setText(percent.format(currentBill.getTipPercent()));
                 tipTextView.setText(currency.format(currentBill.getTipAmount()));
                 totalTextView.setText(currency.format(currentBill.getTotalAmount()));
-
             }
 
             @Override
@@ -65,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Eve
+        // EditText change event
         amountEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -75,7 +78,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Update model
-                currentBill.setAmount(Double.parseDouble(amountEditText.getText().toString()));
+                try {
+                    currentBill.setAmount(Double.parseDouble(s.toString()));
+                } catch (NumberFormatException e){
+                    Log.w("TipCalculator", "NumberFormatException thrown. Attempted to parse double on empty string?");
+                }
+                // Update labels
+                tipTextView.setText(currency.format(currentBill.getTipAmount()));
+                totalTextView.setText(currency.format(currentBill.getTotalAmount()));
             }
 
             @Override
